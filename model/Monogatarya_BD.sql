fa638ee7fd91fe0ba6116a19d0d957f34c54b22d
Akashdeep Singh <akah47.singh@gmail.com>
2026-04-14 13:44:29 +0200

    Refactor database schema

--- model/Monogatarya_BD.sql
+++ model/Monogatarya_BD.sql
@@ -23,26 +23,26 @@ CREATE TABLE IF NOT EXISTS Users (
 
 -- Create Events table --
 CREATE TABLE IF NOT EXISTS Events (
-    ID_Events VARCHAR(25),
+    ID_Event VARCHAR(25),
     Name VARCHAR(50),
     Description VARCHAR(100),
     Date DATE,
     Location VARCHAR(100),
-    CONSTRAINT PK_Events PRIMARY KEY (ID_Events)
+    CONSTRAINT PK_Events PRIMARY KEY (ID_Event)
 );
 
 -- Create Catalogs table (M-M Users <-> Events) --
 CREATE TABLE IF NOT EXISTS Catalogs (
     email VARCHAR(50),
-    ID_Events VARCHAR(25),
-    CONSTRAINT PK_Catalogs PRIMARY KEY (email, ID_Events),
+    ID_Event VARCHAR(25),
+    CONSTRAINT PK_Catalogs PRIMARY KEY (email, ID_Event),
     CONSTRAINT FK_CV_Users FOREIGN KEY (email) REFERENCES Users(email) ON DELETE CASCADE,
-    CONSTRAINT FK_CV_Events FOREIGN KEY (ID_Events) REFERENCES Events(ID_Events) ON DELETE CASCADE
+    CONSTRAINT FK_CV_Events FOREIGN KEY (ID_Event) REFERENCES Events(ID_Event) ON DELETE CASCADE
 );
 
 -- Create Animes table --
 CREATE TABLE IF NOT EXISTS Animes (
-    IDAnimes INT AUTO_INCREMENT,
+    ID_Anime INT AUTO_INCREMENT,
     Title VARCHAR(25),
     Subtitle VARCHAR(100),
     EpisodeCount INT,
@@ -50,11 +50,12 @@ CREATE TABLE IF NOT EXISTS Animes (
     Image VARCHAR(500),
     Video VARCHAR(100),
     Studio VARCHAR(25),
+    Date_premiere DATE DEFAULT CURRENT_DATE,
     Gender VARCHAR(50),
     Description VARCHAR(500),
     email VARCHAR(50),
     -- FK --
-    CONSTRAINT PK_Animes PRIMARY KEY (IDAnimes),
+    CONSTRAINT PK_Animes PRIMARY KEY (ID_Anime),
     CONSTRAINT FK_Users_Animes FOREIGN KEY (email) REFERENCES Users(email) ON DELETE
     SET
         NULL
@@ -62,25 +63,26 @@ CREATE TABLE IF NOT EXISTS Animes (
 
 -- Create Episodes table --
 CREATE TABLE IF NOT EXISTS Episodes (
-    IDEpisodes INT AUTO_INCREMENT,
+    ID_Episode INT AUTO_INCREMENT,
     AnimeName VARCHAR(25),
     Title VARCHAR(50),
     Description VARCHAR(100),
     Link VARCHAR(500),
     EpisodeNumber INT,
-    IDAnimes INT,
+    IDAnime INT,
     -- FK --
-    CONSTRAINT PK_Episodes PRIMARY KEY (IDEpisodes),
-    CONSTRAINT FK_Animes_Episodes FOREIGN KEY (IDAnimes) REFERENCES Animes(IDAnimes) ON DELETE CASCADE
+    CONSTRAINT PK_Episodes PRIMARY KEY (ID_Episode),
+    CONSTRAINT FK_Animes_Episodes FOREIGN KEY (ID_Anime) REFERENCES Animes(ID_Anime) ON DELETE CASCADE
 );
 
 -- Create Mangas table --
 CREATE TABLE IF NOT EXISTS Mangas (
-    IDMangas INT AUTO_INCREMENT,
+    ID_Manga INT AUTO_INCREMENT,
     Title VARCHAR(25),
     Subtitle VARCHAR(25),
     ChapterNumber INT,
     Duration INT,
+    Date_premiere DATE DEFAULT CURRENT_DATE,
     Image VARCHAR(100),
     Video VARCHAR(100),
     Studie VARCHAR(25),
@@ -88,7 +90,7 @@ CREATE TABLE IF NOT EXISTS Mangas (
     Description VARCHAR(100),
     email VARCHAR(50),
     -- FK --
-    CONSTRAINT PK_Mangas PRIMARY KEY (ID_Mangas),
+    CONSTRAINT PK_Mangas PRIMARY KEY (ID_Manga),
     CONSTRAINT FK_Users_Mangas FOREIGN KEY (email) REFERENCES Users(email) ON DELETE
     SET
         NULL
@@ -96,17 +98,17 @@ CREATE TABLE IF NOT EXISTS Mangas (
 
 -- Create Chapters table --
 CREATE TABLE IF NOT EXISTS Chapters (
-    IDChapters INT AUTO_INCREMENT,
+    ID_Chapter INT AUTO_INCREMENT,
     MangaName VARCHAR(50),
     Title VARCHAR(50),
     ChapterNumber INT,
     Description VARCHAR(100),
     Link VARCHAR(500),
     PageCount INT,
-    ID_Mangas INT,
+    ID_Manga INT,
     -- FK --
-    CONSTRAINT PK_Chapters PRIMARY KEY (ID_Chapters),
-    CONSTRAINT FK_Mangas_Chapters FOREIGN KEY (ID_Mangas) REFERENCES Mangas(ID_Mangas) ON DELETE CASCADE
+    CONSTRAINT PK_Chapters PRIMARY KEY (ID_Chapter),
+    CONSTRAINT FK_Mangas_Chapters FOREIGN KEY (ID_Manga) REFERENCES Mangas(ID_Manga) ON DELETE CASCADE
 );
 
 -- Crear procedure de comprovar el email si exite o no --
@@ -142,11 +144,8 @@ SELECT
 
 END / / DELIMITER;
 
-
 -- CREAR ANIME --
-
-DELIMITER //
-CREATE PROCEDURE sp_crearAnime(
+DELIMITER / / CREATE PROCEDURE sp_crearAnime(
     IN p_Title VARCHAR(25),
     IN p_Subtitle VARCHAR(25),
     IN p_EpisodeCount INT,
@@ -157,9 +156,9 @@ CREATE PROCEDURE sp_crearAnime(
     IN p_Gender VARCHAR(50),
     IN p_Description VARCHAR(100),
     IN p_ID_User VARCHAR(50)
-)
-BEGIN
-    INSERT INTO Animes (
+) BEGIN
+INSERT INTO
+    Animes (
         Title,
         Subtitle,
         EpisodeCount,
@@ -170,7 +169,9 @@ BEGIN
         Gender,
         Description,
         ID_User
-    ) VALUES (
+    )
+VALUES
+    (
         p_Title,
         p_Subtitle,
         p_EpisodeCount,
@@ -182,5 +183,5 @@ BEGIN
         p_Description,
         p_ID_User
     );
-END //
-DELIMITER ;
+
+END / / DELIMITER;
