592db2f9b00b734998797922419ee04e738eaa67
Akashdeep Singh <akah47.singh@gmail.com>
2026-04-15 11:00:38 +0200

    Fix file paths for assets in clean_header.php

--- view/includes/clean_header.php
+++ view/includes/clean_header.php
@@ -2,16 +2,16 @@
         <div class="header-gruop">
             <label for="menu-toggle" class="icon-btn white" aria-label="Menú">
                 <svg class="icon">
-                    <use href="../assets/img/icon-sprites.svg#menu"></use>
+                    <use href="/DAM-Transversal/view/assets/img/icon-sprites.svg#menu"></use>
                 </svg>
             </label>
-            <img src="../assets/img/logo.webp" alt="Logo de la página">
+            <img src="/DAM-Transversal/view/assets/img/logo.webp" alt="Logo de la página">
             <h1>MONOGATARYA</h1>
             <div class="right-group">
 
                 <a href="profile_promotor-view.html" class="icon-btn white user-link" aria-label="Ir al perfil de promotor">
                     <svg class="icon">
-                        <use href="../assets/img/icon-sprites.svg#usuario"></use>
+                        <use href="/DAM-Transversal/view/assets/img/icon-sprites.svg#usuario"></use>
                     </svg>
                 </a>
             </div>
