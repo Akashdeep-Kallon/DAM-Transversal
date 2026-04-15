905d2881a9639b3a36b78295ef478d382b6a5bad
Akashdeep Singh <akah47.singh@gmail.com>
2026-04-15 10:55:27 +0200

    Fix file path for header inclusion in anime catalog and index pages

--- view/index.php
+++ view/index.php
@@ -11,7 +11,7 @@
 </head>
 
 <body>
-    <?php require '../includes/clean_header.php'; ?>
+    <?php require 'includes/clean_header.php'; ?>
 
     <main class="page-main" id="contenido-principal">
         <div class="layout-container">
