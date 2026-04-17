e2b3bf0916ada9b26acec493a68b5bb01191af99
Sergi <sergiye787@gmail.com>
2026-04-17 10:35:53 +0200

    Redirijir Inicio sesion

--- view/includes/clean-header.php
+++ view/includes/clean-header.php
@@ -13,11 +13,19 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/DAM-Transversal/config.php';
             </a>
             <h1>MONOGATARYA</h1>
             <div class="right-group">
+                <?php if (isset($_SESSION['usuario'])): ?>
                 <a href="/DAM-Transversal/view/profiles/profile.php" class="icon-btn white user-link" aria-label="Ir al perfil de promotor">
                     <svg class="icon">
                         <use href="/DAM-Transversal/view/assets/img/icon-sprites.svg#usuario"></use>
                     </svg>
                 </a>
+                <?php else: ?>
+                    <a href="/DAM-Transversal/view/auth/login.php" class="icon-btn white user-link" aria-label="Iniciar sesión">
+                        <svg class="icon">
+                            <use href="/DAM-Transversal/view/assets/img/icon-sprites.svg#usuario"></use>
+                        </svg>
+                    </a>
+                <?php endif; ?>
             </div>
         </div>
     </header>
